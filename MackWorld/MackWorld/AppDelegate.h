aad92ee42b0f04469b15f0f84325125667907360
minhechen <minhechen@gmail.com>
2013-09-17 10:23:34 +0800

    hello world

---
+++ MackWorld/MackWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MackWorld
+//
+//  Created by minhechen on 13-9-17.
+//  Copyright (c) 2013年 minhechen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MackWorld/MackWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MackWorld
+//
+//  Created by minhechen on 13-9-17.
+//  Copyright (c) 2013年 minhechen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
